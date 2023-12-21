21bdbe572ecd3ec3d7fd011ce40184a05861e261
Tristan <tristanpedro02@gmail.com>
2023-12-21 14:33:03 -0600

    fix incorrect code upload

--- app.py
+++ app.py
@@ -23,14 +23,24 @@ CNN = None  # global variable to hold the CNN model
 def load_model():
     global CNN
     script_dir = os.path.dirname(__file__)
-    model_path = os.path.join(script_dir, 'models', 'brain_tumor_cnn_classifier.keras')
+    model_json_path = os.path.join(script_dir, 'models', 'CNN_structure.json')
 
+    with open(model_json_path, 'r') as json_file:
+        model_json = json_file.read()
     try:
-        CNN = tf.keras.models.load_model(model_path, compile=False)
+        # load model
+        CNN = tf.keras.models.model_from_json(model_json)
+
+        # load and set model weights
+        weights_path = os.path.join(script_dir, 'models', 'CNN_weights.pkl')
+        with open(weights_path, 'rb') as weights_file:
+            weights = pickle.load(weights_file)
+            CNN.set_weights(weights)
+
+        # compile model
         CNN.compile(optimizer=tf.keras.optimizers.Adamax(learning_rate=0.001), 
                     loss='categorical_crossentropy', 
                     metrics=['accuracy'])
-        logger.info("Model loaded successfully.")
     except Exception as e:
         logger.error(f"Error loading model: {e}")
 
@@ -38,21 +48,25 @@ with app.app_context():
     logger.info("Loading model...")
     load_model()
 
+# function for retrieving prediction from model given an image path
 def get_model_prediction(image_path):
     try:
         # load and preprocess the image
         img = Image.open(image_path).resize((224, 224))
+        # convert grayscale images to RGB
         if img.mode != 'RGB':
             img = img.convert('RGB')
+        
         img_array = np.expand_dims(np.array(img), axis=0)
-
+        
         # predict using the CNN model
         prediction = CNN.predict(img_array)
-
+        
         # interpret the prediction
         predicted_index = np.argmax(prediction[0])
         class_labels = ['glioma', 'meningioma', 'no tumor', 'pituitary']
-        return class_labels[predicted_index]
+        predicted_class = class_labels[predicted_index]
+        return predicted_class
     except Exception as e:
         logger.error(f"Error in get_model_prediction: {e}")
         return None
